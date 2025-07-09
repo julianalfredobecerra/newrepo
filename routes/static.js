aa95f74016dc9689c05489bbb050631e07fb1017
julianbrom1 <bec21001@byui.edu>
2025-07-08 22:24:35 -0500

    try

--- routes/static.js
+++ routes/static.js
@@ -2,11 +2,8 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-// Correct static paths
+// Serve everything from /public
 router.use(express.static(path.join(__dirname, '../public')));
-router.use("/css", express.static(path.join(__dirname, '../public/css')));
-router.use("/js", express.static(path.join(__dirname, '../public/js')));
-router.use("/images", express.static(path.join(__dirname, '../public/images')));
 
 module.exports = router;
 
